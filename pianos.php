a433b3e35b775ebc847b847a20df073c464d8134
Furkan Durmaz <muhammedfurkandurmaz@gmail.com>
2019-09-27 16:29:11 +0200

    Added cards dynamically using piano array

--- pianos.php
+++ pianos.php
@@ -1,5 +1,9 @@
 <?php
 include('assets/library/start.php');
+include_once 'assets/content/stand-alone-producten/producten.inc.php';
+
+// Load the array with pianos
+$piano_array = [];
 ?>
 
 <div id="header-piano" class="jumbotron jumbotron-fluid header">
@@ -11,86 +15,19 @@ include('assets/library/start.php');
 
 <div class="container">
     <div class="row justify-content-center justify-content-md-between flex-wrap">
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Steinway-145-(1920)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Steinway</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
+        <?php foreach ($items as $key => $item) {
+            echo " <div class=\"col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center\">
+            <div class=\"card shadow\" style=\"width: 18rem;\">
+                <img src=\"./assets/content/stand-alone-producten/images/pianos/Steinway-145-(1920)-1-1000x1000.jpg\" class=\"card-img-top\">
+                <div class=\"card-body\">
+                    <h5 class=\"card-title\">{$item['merk']}</h5>
+                    <p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href=\"product-info.php\" class=\"card-link\">Bekijk deze piano <span class=\"fas fa-arrow-right\"></span></a>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Seiler-128-Smr-(1993)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Seiler</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Steinbeck-XU-132HA-13266-(2006)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Steinbeck</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Kawai-KS-3F-12561-(1984)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Kawai</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Gebr-Schulze-115-(2001)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Gebr. Schulze</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/H-Mayer-11256-(2003)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">H. Mayer</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Rippen-10753-(1953)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Rippen</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 my-3 d-flex justify-content-center mr-xl-auto">
-            <div class="card shadow" style="width: 18rem;">
-                <img src="./assets/content/stand-alone-producten/images/pianos/Roesler-10853-(1963)-1-1000x1000.jpg" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Rösler</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="product-info.php" class="card-link">Bekijk deze piano <span class="fas fa-arrow-right"></span></a>
-                </div>
-            </div>
-        </div>
+        </div> ";
+        }
+        ?>
     </div>
 </div>
 <?php
