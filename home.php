3d61c73c330319e1dc5b48849d62fb5f8c691a64
Furkan Durmaz <muhammedfurkandurmaz@gmail.com>
2019-10-02 16:15:18 +0200

    - Deleted padding added through JS - Added extra responsive breakpoint for tablet

--- home.php
+++ home.php
@@ -12,44 +12,59 @@ include('assets/library/start.php');
 <div class="container">
     <div class="row">
         <div class="d-flex justify-content-lg-between justify-content-center flex-wrap">
-            <div class="col-12 col-lg-3 text-center my-3 my-lg-0">
+            <div class="col-12 col-md-6 col-lg-3 text-center my-3 my-lg-0">
                 <a href="pianos.php"> <i class="fas fa-store-alt service-icon"></i></a>
                 <h2 class="pt-2">Piano’s en vleugels</h2>
-                <p><br>
-                    We hebben altijd zo’n 15 tot 20 gebruikte piano’s en vleugels in onze showroom staan in
-                    verschillende prijsklassen.<br><br>
-                    Er staat voor u wellicht ook een piano of vleugel tussen. Bekijk ons aanbod
-                    <a href="pianos.php">piano’s</a> en <a href="vleugels.php">vleugels</a>
+                <p>
+                    We hebben altijd zo’n 15 tot 20 gebruikte piano’s en vleugels in onze showroom staan in verschillende prijsklassen, dus er staat voor u wellicht ook een piano of vleugel tussen.
+                    <br>
+                    Bekijk ons aanbod <a href="pianos.php">piano's</a> en <a href="pianos.php">vleugels</a>. <br><br>
+                    Ook kunt u bij ons terecht als u een nieuwe piano of vleugel zoekt. We zijn de Nederlandse dealer van Feurich. U kunt diverse modellen bij ons proberen.
+                    <br>
+                    We nemen graag alle tijd voor u.
+                    <br><br>
+                    <a href="contact.php">Maak nu een afspraak <span class="fas fa-arrow-right"></span></a>
+
                 </p>
             </div>
-            <div class="col-12 col-lg-3 text-center my-3 my-lg-0">
+            <div class="col-12 col-md-6 col-lg-3 text-center my-3 my-lg-0">
                 <a href="over-ons.php"> <i class="fas fa-music service-icon"></i></a>
-                <h2 class="pt-2">Over Sloot Piano<wbr>Service</h2>
-                <p><br>
-                    We hebben altijd zo’n 15 tot 20 gebruikte piano’s en vleugels in onze showroom staan in
-                    verschillende prijsklassen.<br><br>
-                    Er staat voor u wellicht ook een piano of vleugel tussen. Bekijk ons aanbod
-                    <a href="pianos.php">piano’s</a> en <a href="vleugels.php">vleugels</a>
+                <h2 class="pt-2">Over Sloot Pianoservice</h2>
+                <p>
+                    Sloot Pianoservice is opgericht in 1984. Wij zijn een vriendelijk, lokaal bedrijf met een grote passie voor muziek in vele genres. Sloot Pianoservice is gevestigd in Doetinchem.
+                    <br><br>
+                    Onze klanten vinden we door heel Nederland en vooral in de Achterhoek. We bieden een scala aan diensten op het gebeid van verkoop, huur, transport en taxatie voor zowel particulieren, bedrijven en evenementen.
+                    <br><br>
+                    Kom eens langs om uw wensen met ons te bespreken.
                 </p>
             </div>
-            <div class="col-12 col-lg-3 text-center my-3 my-lg-0">
+            <div class="col-12 col-md-6 col-lg-3 text-center my-3 my-lg-0">
                 <a href="revisie.php"> <i class="fas fa-tools service-icon"></i></a>
                 <h2 class="pt-2">Reparaties en stemmen</h2>
-                <p><br>
-                    We hebben altijd zo’n 15 tot 20 gebruikte piano’s en vleugels in onze showroom staan in
-                    verschillende prijsklassen.<br><br>
-                    Er staat voor u wellicht ook een piano of vleugel tussen. Bekijk ons aanbod
-                    <a href="pianos.php">piano’s</a> en <a href="vleugels.php">vleugels</a>
+                <p>
+                    We komen graag bij u langs om uw piano of vleugel te stemmen.
+                    Daarnaast zijn we gespecialiseerd in het repareren en reviseren van piano’s en vleugels.
+                    <br><br>
+                    We doen dat in onze uitgebreide, eigen werkplaats.
+                    We reviseren instrumenten voor de verkoop, maar ook in opdracht van de eigenaar.
+                    <br><br>
+                    <a href="stemmen.php">Bekijk onze tarieven <span class="fas fa-arrow-right"></span></a>
                 </p>
             </div>
-            <div class="col-12 col-lg-3 text-center my-3 my-lg-0">
+            <div class="col-12 col-md-6 col-lg-3 text-center my-3 my-lg-0">
                 <a href="transport.php"> <i class="fas fa-truck service-icon"></i></a>
                 <h2 class="pt-2">Verhuur en transport</h2>
-                <p><br>
-                    We hebben altijd zo’n 15 tot 20 gebruikte piano’s en vleugels in onze showroom staan in
-                    verschillende prijsklassen.<br><br>
-                    Er staat voor u wellicht ook een piano of vleugel tussen. Bekijk ons aanbod
-                    <a href="pianos.php">piano’s</a> en <a href="vleugels.php">vleugels</a>
+                <p>
+                    Wilt u een piano huren voor een bijzonder gelegenheid of concert?
+                    We hebben daarvoor diverse instrumenten staan. Kom gerust eens langs. Er staat beslist een passend instrument voor u.
+                    <br>
+                    <a href="huren.php">Bekijk de tarieven</a> voor de huur van een piano of vleugel.
+                    <br><br>
+                    Gaat u verhuizen of heeft u elders een piano of vleugel gekocht? We verzorgen graag voor u het transport.
+                    <br>
+                    In onze 35-jarig bestaan zijn we de expert in het vervoeren van piano’s en vleugels.
+                    <br><br>
+                    <a href="transport.php">Meer over transport <span class="fas fa-arrow-right "></span></a>
                 </p>
             </div>
 
