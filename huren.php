a47d2e460edae1975b6ba265ea6ac1186ec22510
Stephan timmer <47012392+Akenixx@users.noreply.github.com>
2019-10-07 16:41:36 +0200

    worked on stemmen.php, created layout.

--- huren.php
+++ huren.php
@@ -10,24 +10,23 @@ include('assets/library/start.php');
 
 <div class="container">
     <div class="row justify-content-center justify-content-md-between flex-wrap">
-        <p class="col-12 text-center col-md-8 text-md-left col-lg-6 order-0">Wilt u een piano/vleugel huren voor een feest,
+        <p class="col-12 text-left col-md-6 col-lg-4">Wilt u een piano/vleugel huren voor een feest,
             een concert of een andere bijzondere gebeurtenis?
             Dan hebben wij voor dit soort gelegenheden een diverse instrumenten staan. Neem contact met ons op per
             telefoon (06-xxxxx)
             of via de contactpagina [link naar de contactpagina] en maak een passende en verantwoorde keuze uit ons
             aanbod.
         </p>
-        <img class="justify-content-center justify-content-md-between col-12 col-md-4 col-lg-4" src="https://via.placeholder.com/350x250" alt="placeholder">
-
-        <div class="row justify-content-center justify-content-md-between">
-            <img src="https://via.placeholder.com/350x250" alt="" class="col-12 col-md-4 col-lg-4">
-            <p class="col-12 text-center col-md-8 text-md-left col-lg-6">
-                Dit tarief is inclusief transportkosten binnen Doetinchem en het stemmen van het instrument
-                Buiten Doetinchem geldt een aangepast tarief
-            </p>
-        </div>
+        <img class="col-12 col-md-6 col-lg-4 order-md-2" src="https://via.placeholder.com/350x250" alt="placeholder">
     </div>
-</div>
-<?php
-include('assets/library/end.php');
-?>
+
+    <div class="row justify-content-center justify-content-md-between flex-wrap">
+        <img class="col-12 col-md-6 col-lg-4 order-3 order-md-0" src="https://via.placeholder.com/350x250" alt="placeholder">
+        <p class="col-12 text-left col-md-6 text-md-left col-lg-4">
+            Dit tarief is inclusief transportkosten binnen Doetinchem en het stemmen van het instrument
+            Buiten Doetinchem geldt een aangepast tarief
+        </p>
+    </div>
+    <?php
+    include('assets/library/end.php');
+    ?>
--- stemmen.php
+++ stemmen.php
@@ -9,6 +9,49 @@ include('assets/library/start.php');
     </div>
 </div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <p>
+                Het is belangrijk dat een piano of vleugel regelmatig gestemd wordt.
+                Dan blijft deze op de juiste toonhoogte en worden eventuele onzuivere noten gecorrigeerd.
+                Iedere noot bestaat uit meerdere snaren die tezamen het geluid geven.
+                Omdat het instrument een natuurproduct is, verlopen de snaren langzaam maar zeker.
+                Daarom is regelmatig stemmen nodig.<br><br>
+
+                Het aantal keer per jaar dat een piano of vleugel gestemd moet worden is afhankelijk van de
+                speelintensiteit, hoe veel en hoe intensief er op gespeeld wordt, en de stemvastheid,
+                die vooral wordt bepaald door het stemblok (zie afbeelding)<br><br>
+
+                We geven u graag advies over hoe vaak uw piano of vleugel gestemd wordt. <br><br>
+
+                Wanneer een piano of vleugel langere tijd niet gestemd is,
+                dan zakt de toonhoogte van het instrument. Dat betekent dat het gehele instrument op toon
+                getrokken moet worden, om weer goed te klinken. Vooral als u ook met andere instrumenten
+                samenspeelt is dat een vereiste. Het op toon trekken van een piano of vleugel is vaak een
+                taak van wat langere adem: in één tot meerdere stembeurten verhogen we de toon steeds iets
+                meer, om te voorkomen dat er in één keer te veel spanning op het stemblok komt te staan<br><br> waardoor het blok kan gaan scheuren of andere mankementen gaat vertonen Ook hierover geven we u graag en deskundig advies.
+            </p>
+        </div>
+        <div class="col-12 col-md-6 bg-dark">
+            <div class="row justify-content-center d-flex flex-wrap">
+                <img class="col-10 my-3" src="assets/images/stemmen-rightblock.jpg">
+                <p class="col-10 text-rightblock-stemmen ">
+                Moet uw piano of vleugel gestemd worden? Neem dan contact met ons op. 
+                Dan maken we zo snel mogelijk een afspraak met u om te stemmen.
+                Dat kan per telefoon:<br><br>
+                &bull; Eric<a href="tel:+31622187427"> (+31) 622187427</a><br>
+                &bull; Rutger: <a href="tel:+31629052155"> (+31) 629052155</a><br><br>
+                of vul het <a href="contact.php">contactformulier</a> in.
+                </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+
 <?php
 include('assets/library/end.php');
 ?>
