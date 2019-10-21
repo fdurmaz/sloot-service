10fc7322f21142dae9818deb505ad72ae02196c0
Stephan timmer <47012392+Akenixx@users.noreply.github.com>
2019-10-21 09:39:18 +0200

    readded content for transport.php

--- view/transport.php
+++ view/transport.php
@@ -1,6 +1,71 @@
+<?php
+include('view/template/head.php ');
+?>
+
 <div id="header-transport" class="jumbotron jumbotron-fluid header">
     <div class="container">
         <h1 class="display-3">Transport</h1>
         <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed augue</p>
     </div>
 </div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <p>
+                Het transport van een piano of vleugel vraagt om speciale aanpak. Het instrument moet daarbij zo min mogelijk ‘lijden’. Al meer dan 35 jaar verhuizen wij piano’s en vleugels naar volle tevredenheid van onze klanten.<br><br>
+                Of het nu gaat om een door u particulier aangekochte piano of vleugel, om de tijdelijke plaatsing van een gehuurd instrument, of als u zelf gaat verhuizen en de piano of vleugel naar uw nieuwe huis gaat, alle gelijkvloerse transporten verzorgen we geheel zelf. Voor transporten van of naar een verdieping verwijzen we u door naar een erkend verhuizer. Deze beschikt daarvoor over alle noodzakelijke hulpmiddelen. Uiteraard is uw instrument tijdens het transport goed verzekerd.<br><br>
+            </p>
+        </div>
+        <div class="col-12 col-md-8">
+            <h2>Tarieven</h2>
+            <p>Voor het verhuizen en overig transport hanteren we de volgende tarieven:
+
+                <table class="table">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Binnen Regio Doetinchem</th>
+                        <th scope="col">Buiten Regio Doetinchem</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Piano:</th>
+                        <td> € 125,-</td>
+                        <td>€ 150,-</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Vleugel:</th>
+                        <td> € 200,-</td>
+                        <td>€ 250,-</td>
+                    </tr>
+                </table>
+
+
+                Dit betreft het starttarief voor het eerste uur.<br>
+                Na het starttarief word er € 60,- per uur extra in kosten gebracht.<br><br>
+
+                Bel ons gerust om een prijsopgave of om meteen een afspraak te maken:<br></p>
+            <ul class="list-unstyled">
+                <li class="media">
+                    <img src="https://via.placeholder.com/64" class="mr-3" alt="Eric sloot">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Eric</h5>
+                        06-22187427
+                    </div>
+                </li> 
+                
+                <li class="media my-2">
+                    <img src="https://via.placeholder.com/64" class="mr-3" alt="Eric sloot">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1">Rutger</h5>
+                        06-22187427
+                    </div>
+                </li>
+               <li><a href="contact">Of vul het contactformulier in<span class="fas fa-arrow-right" </a><li>
+            </ul>
+        </div>
+    </div>
+
+    <?php
+    include('view/template/footer.php');
+    ?>
