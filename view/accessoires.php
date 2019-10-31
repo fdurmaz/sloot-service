e5c4f1cddb30f1db68de3c3142a434d2a2df0e92
Stephan timmer <47012392+Akenixx@users.noreply.github.com>
2019-10-31 13:37:03 +0100

    Accessiores.php (right version)
    Previous version of Accesoires.php was uploaded with mistakes in it. this is the good one

---
+++ view/accessoires.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'assets/library/products.inc.php';
+?>
+
+<div id="header-piano" class="jumbotron jumbotron-fluid header">
+    <div class="container">
+        <h1 class="display-3">accesoires</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed augue.</p>
+    </div>
+</div>
+
+
+<?php
+$soort = "accessoires";
+include_once 'assets/library/products-main.php';
+
+    
