6bcbec98f4dc5d62dfc10350cd01c3377469bd41
Stephan timmer <47012392+Akenixx@users.noreply.github.com>
2019-10-31 14:32:22 +0100

    changed Revisie.php > Reparatie.php, fixed reparatie.php

---
+++ view/reparatie.php
@@ -0,0 +1,32 @@
+<div id="header-reparatie" class="jumbotron jumbotron-fluid header">
+    <div class="container">
+        <h1 class="display-3">Reparatie</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed augue</p>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-7">
+            <div class="col-9">
+                <p>Het repareren of reviseren van een piano of vleugel vraagt om passie en expertise. Passie om door te gaan tot het instrument weer in uitmuntende staat is, expertise van de technieken waaruit piano’s en vleugels bestaan.<br><br>
+
+                    Eric Sloot heeft meer dan 35 ervaring in het repareren en reviseren van piano’s en vleugels. Ook Rutger is inmiddels zeer deskundig in het vak. Beide zijn ze van huis uit zeer gedreven om van ieder instrument dat ze onderhanden nemen, weer een topinstrument te maken. <br><br>
+
+                    Neem gerust contact met ons op om en mogelijke revisie of reparatie van uw piano of vleugel door ons te laten beoordelen en begroten. We bespreken graag met u de mogelijkheden en de kosten/baten voor u. We maken graag een gerichte offerte, zodat u weet wat u te wachten staat.
+                </p>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="col-11">
+                <img class="my-1" src="https://via.placeholder.com/125" alt="">
+                <img class="m-1" src="https://via.placeholder.com/125" alt="">
+                <img class="my-1" src="https://via.placeholder.com/125" alt="">
+                <img class="m-1" src="https://via.placeholder.com/125" alt="">
+            </div>
+            <a class="m-5" href="contact.php">vul het contactformulier in <span class="fas fa-arrow-right"></a>
+        </div>
+
+    </div>`
+</div>
